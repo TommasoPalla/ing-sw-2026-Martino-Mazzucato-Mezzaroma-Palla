4e869958adf77760fb9a39a017f912a7ec24a06b
Lorenzo Mazzucato <162993212+lorenzomazzucato@users.noreply.github.com>
2026-04-04 13:05:41 +0200

    Update InventorType.java

--- src/main/java/enums/InventorType.java
+++ src/main/java/enums/InventorType.java
@@ -1,5 +1,15 @@
 package enums;
-
+/*
+this enum represents all the possible types related to the inventor character*/
 public enum InventorType {
-  BOAT, ARROW, HOOK, NECKLACE, BOWL, ROPE, DOLL, FLUTE, LEATHER, BREAD;
+  BOAT,
+  ARROW, 
+  HOOK, 
+  NECKLACE, 
+  BOWL, 
+  ROPE, 
+  DOLL, 
+  FLUTE, 
+  LEATHER, 
+  BREAD;
 }
