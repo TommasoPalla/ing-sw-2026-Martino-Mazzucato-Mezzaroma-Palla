006b9a909e54a25f2de3cd3422ac22bb39984d1c
Lorenzo Mazzucato <162993212+lorenzomazzucato@users.noreply.github.com>
2026-04-04 13:07:07 +0200

    Update EventType.java

--- src/main/java/enums/EventType.java
+++ src/main/java/enums/EventType.java
@@ -3,5 +3,8 @@ package enums;
 this enum represents all the possible types of event*/
 
 public enum EventType {
-SUSTENANCE, HUNT, SHAMANIC_RITUAL, CAVE_PAINTINGS;
+  SUSTENANCE, 
+  HUNT, 
+  SHAMANIC_RITUAL, 
+  CAVE_PAINTINGS;
 }
