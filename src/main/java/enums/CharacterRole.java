e528d7d6eb0fee1570bd5e0c7638289072c99173
Lorenzo Mazzucato <162993212+lorenzomazzucato@users.noreply.github.com>
2026-04-04 13:07:53 +0200

    Update CharacterRole.java

--- src/main/java/enums/CharacterRole.java
+++ src/main/java/enums/CharacterRole.java
@@ -3,5 +3,10 @@ package enums;
 this enum represents all the possible roles of a character*/
 
 public enum CharacterRole {
-  HUNTER, BUILDER, GATHERER, ARTIST, SHAMAN, INVENTOR;
+  HUNTER, 
+  BUILDER, 
+  GATHERER, 
+  ARTIST, 
+  SHAMAN, 
+  INVENTOR;
 }
