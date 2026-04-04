e9d0b545dd0df50c4bf44145068f4526c2452442
Lorenzo Mazzucato <162993212+lorenzomazzucato@users.noreply.github.com>
2026-04-04 13:04:26 +0200

    Update GamePhase.java

--- src/main/java/enums/GamePhase.java
+++ src/main/java/enums/GamePhase.java
@@ -1,5 +1,7 @@
 package enums;
-
+/*
+this enum represents all the possible game phases that could trigger the effect of a building, the activation of an event 
+or the general administration of the game */
 public enum GamePhase {
 ON_DRAW, END_GAME, ON_EVENT, END_TURN, START_TURN;
 }
