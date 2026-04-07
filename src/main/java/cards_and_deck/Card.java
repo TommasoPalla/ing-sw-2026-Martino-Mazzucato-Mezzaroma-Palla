39e001e25fb790d52d24607a48e20d46998ff797
PaoloMartino <paolo2.martino@mail.polimi.it>
2026-04-07 18:26:31 +0200

    Card Class

--- src/main/java/cards_and_deck/Card.java
+++ src/main/java/cards_and_deck/Card.java
@@ -1,4 +1,25 @@
 package cards_and_deck;
 
+import enums.Era;
+
 public abstract class Card {
+    private final Era era;
+    private final String name;
+    private final String cardID;
+
+    public Card(Era era, String name, String cardID){
+      this.era = era;
+      this.name = name;
+      this.cardID = cardID;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getCardID(){
+        return this.cardID;
+    }
+    public Era getEra(){
+        return this.era;
+    }
 }
