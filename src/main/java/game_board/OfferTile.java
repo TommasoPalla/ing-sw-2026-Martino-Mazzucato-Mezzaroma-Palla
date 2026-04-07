5061fdc16a5130db45bd96e4a706b270284655ce
Davide Mezzaroma <davidemezzaroma67@gmail.com>
2026-04-07 16:42:24 +0200

    OfferTile fatta, da finire la OfferTile causa mancanza Deck

--- src/main/java/game_board/OfferTile.java
+++ src/main/java/game_board/OfferTile.java
@@ -1,4 +1,69 @@
 package game_board;
+import users.Player;
 
 public class OfferTile {
+    private int cardsFromAbove;
+    private int cardsFromBelow;
+    private int foodBonus;
+
+    private boolean occupied;
+    protected Player currentOccupant;
+
+    OfferTile(String cardID){
+        this.occupied = false;
+        this.currentOccupant = null;
+        switch (cardID){
+            case "OT_A":
+                this.cardsFromAbove = 0;
+                this.cardsFromBelow = 0;
+                this.foodBonus = 3;
+                break;
+            case "OT_B":
+                this.cardsFromAbove = 0;
+                this.cardsFromBelow = 1;
+                this.foodBonus = 0;
+                break;
+            case "OT_C":
+                this.cardsFromAbove = 1;
+                this.cardsFromBelow = 0;
+                this.foodBonus = 0;
+                break;
+            case "OT_D":
+                this.cardsFromAbove = 0;
+                this.cardsFromBelow = 2;
+                this.foodBonus = 0;
+                break;
+            case "OT_E":
+                this.cardsFromAbove = 1;
+                this.cardsFromBelow = 1;
+                this.foodBonus = 0;
+                break;
+            case "OT_F":
+                this.cardsFromAbove = 2;
+                this.cardsFromBelow = 0;
+                this.foodBonus = 0;
+                break;
+            case "OT_G":
+                this.cardsFromAbove = 2;
+                this.cardsFromBelow = 1;
+                this.foodBonus = 0;
+                break;
+        }
+    }
+
+    //getters
+    public int getCardsFromAbove() {return cardsFromAbove;}
+    public int getCardsFromBelow(){return cardsFromBelow;}
+    public boolean isOccupied(){return occupied;}
+    public int getFoodBonus(){return foodBonus;}
+
+    //actual functions
+    public void occupy(Player player){
+        currentOccupant = player;
+        occupied = true;
+    }
+    public void free(){
+        currentOccupant = null;
+        occupied = false;
+    }
 }
--- src/main/java/game_board/OfferTrack.java
+++ src/main/java/game_board/OfferTrack.java
@@ -1,4 +1,84 @@
 package game_board;
 
-public class OfferTrack {
+import cards_and_deck.BuildingCard;
+import cards_and_deck.Card;
+import cards_and_deck.EventCard;
+
+import java.util.ArrayList;
+
+public class OfferTrack{
+    private ArrayList<OfferTile> offerTiles;
+    private ArrayList<Card> topRow;
+    private ArrayList<Card> bottomRow;
+    private ArrayList<BuildingCard> topBuildingCard;
+    private ArrayList<BuildingCard> bottomBuildingCard;
+    private int currentPlayerTileIdx;
+
+    OfferTrack(int playerNumber){
+
+        this.offerTiles = new ArrayList<OfferTile>();
+        this.topRow = new ArrayList<Card>();
+        this.bottomRow = new ArrayList<Card>();
+        this.topBuildingCard = new ArrayList<BuildingCard>();
+        this.bottomBuildingCard = new ArrayList<BuildingCard>();
+        this.currentPlayerTileIdx = -1;
+
+        if(playerNumber >= 2){
+            offerTiles.add(new OfferTile("OT_B"));
+            offerTiles.add(new OfferTile("OT_C"));
+            offerTiles.add(new OfferTile("OT_E"));
+            offerTiles.add(new OfferTile("OT_F"));
+        }
+        if(playerNumber >= 3) offerTiles.add(new OfferTile("OT_D"));
+        if(playerNumber >= 4) offerTiles.add(new OfferTile("OT_G"));
+        if(playerNumber == 5) offerTiles.add(new OfferTile("OT_A"));
+        if(playerNumber < 2 || playerNumber > 5) System.out.println("invalid number of players"); //maybe throw exception
+    }
+
+    //getters
+    public ArrayList<OfferTile> getOfferTiles() {return offerTiles;}
+    public ArrayList<Card> getTopRow() {return topRow;}
+    public ArrayList<Card> getBottomRow() {return bottomRow;}
+    public ArrayList<BuildingCard> getTopBuildingCard() {return topBuildingCard;}
+    public ArrayList<BuildingCard> getBottomBuildingCard() {return bottomBuildingCard;}
+    public int getCurrentPlayerTileIdx() {return currentPlayerTileIdx;}
+
+    //actual functions
+    public Card removeCardFromTop(int index){
+        Card indexedCard = topRow.get(index);
+        if(indexedCard instanceof EventCard) {
+            //throw exception and choose again
+            System.out.println("You can't draw an event, choose a character or a building if available.");
+            return null;
+        }
+        else return indexedCard;
+    }
+
+    public Card removeCardFromBottom(int index) {
+        Card indexedCard = topRow.get(index);
+        if (indexedCard instanceof EventCard) {
+            //throw exception and choose again
+            System.out.println("You can't draw an event, choose a character or a building if available");
+            return null;
+        }
+        else return indexedCard;
+    }
+
+    public void moveCardsToBottom(){
+        bottomRow = topRow;
+    }
+    /* waiting for deck implementation
+    public void repopulateTopRow(){
+        topRow.add(deck.draw());
+    }
+    */
+    public void moveBuildings(){
+        bottomBuildingCard = topBuildingCard;
+    }
+
+    /* waiting for deck implementation
+    public void repopulateTopBuildingCards(){
+    }
+    */
+
 }
