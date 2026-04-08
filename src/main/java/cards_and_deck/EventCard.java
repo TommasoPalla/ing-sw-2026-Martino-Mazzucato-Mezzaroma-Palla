f515a95bc9fe8de5d3ada6e1d7ed542781d6d0fb
PaoloMartino <paolo2.martino@mail.polimi.it>
2026-04-08 17:46:01 +0200

    eventCard class added

--- src/main/java/cards_and_deck/EventCard.java
+++ src/main/java/cards_and_deck/EventCard.java
@@ -1,4 +1,16 @@
 package cards_and_deck;
 
-public class EventCard extends Card {
+import enums.EventType;
+import enums.Era;
+
+public class EventCard extends Card{
+    private final EventType type;
+
+    public EventCard(Era era, String name, String cardID, EventType type){
+        super(era, name, cardID);
+        this.type = type;
+    }
+    public EventType getType(){
+        return this.type;
+    }
 }
