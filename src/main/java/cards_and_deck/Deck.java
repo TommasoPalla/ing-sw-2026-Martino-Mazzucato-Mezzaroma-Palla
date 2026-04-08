955fc2720972a5ad631fd36e001c7920334c1fea
PaoloMartino <paolo2.martino@mail.polimi.it>
2026-04-08 20:14:00 +0200

    first draft of Deck class

--- src/main/java/cards_and_deck/Deck.java
+++ src/main/java/cards_and_deck/Deck.java
@@ -1,4 +1,40 @@
 package cards_and_deck;
 
+import enums.Era;
+import users.Player;
+
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+
 public class Deck {
+    private ArrayDeque<Card> tribeDeck;
+    private ArrayList<BuildingCard> buildingsDeck;
+    private int deckInitLength;
+
+    //constructor called by Game.startGame()
+    public Deck(int numPlayers){
+        switch (numPlayers){
+            case 2 -> this.deckInitLength = 70;//numero da definire
+            case 3 -> this.deckInitLength = 75;//num
+            case 4 -> this.deckInitLength = 80;//num
+            case 5 -> this.deckInitLength = 85;//num
+        }
+        initTribeDeck(Era.FIRST);
+    }
+    //method called by Game.changeEra(), creates a new deck for the current era
+    public void initTribeDeck(Era era){
+
+    }
+    public Card drawCard(){
+        if(!tribeDeck.isEmpty()) {
+            return tribeDeck.pop();
+        }    //pop lancia noSuchElementException, si potrebbe usare al posto dell'if
+        else {
+            Era e = Game.getEra();
+            initTribeDeck(e);
+        }
+    }
+    public int getCardsNumber(){
+
+    }
 }
