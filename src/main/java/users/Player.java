4c0667aa20fc9e2f87faeb9f494ae12cbd543e83
Davide Mezzaroma <davidemezzaroma67@gmail.com>
2026-04-08 21:56:03 +0200

    Added getters method for Player and Tribe classes

--- src/main/java/users/Player.java
+++ src/main/java/users/Player.java
@@ -1,4 +1,17 @@
 package users;
 
+import enums.Color;
+import game_board.OfferTile;
+
 public class Player {
+    private String name;
+    private Color totemColor;
+    private Tribe tribe;
+    private OfferTile currentOfferTile;
+
+    //getters
+    public String getName() {return name;}
+    public Color getTotemColor() {return totemColor;}
+    public Tribe getTribe() {return tribe;}
+    public OfferTile getCurrentOfferTile() {return currentOfferTile;}
 }
--- src/main/java/users/Tribe.java
+++ src/main/java/users/Tribe.java
@@ -1,4 +1,32 @@
 package users;
 
+import cards_and_deck.BuildingCard;
+import cards_and_deck.CharacterCard;
+
+import java.util.ArrayList;
+
 public class Tribe {
+    private int prestigePoints;
+    private int foodReserve;
+    private ArrayList<CharacterCard> population;
+    private ArrayList<BuildingCard> buildings;
+
+    private int huntersNumber;
+    private int builderDiscount;
+    private int gatherersDiscount;
+    private int artistsNumber;
+    private int shamanStars;
+    private int inventorsNumberPerType;
+
+    //getters
+    public int getPrestigePoints() {return prestigePoints;}
+    public int getFoodReserve() {return foodReserve;}
+    public ArrayList<CharacterCard> getPopulation() {return population;}
+    public ArrayList<BuildingCard> getBuildings() {return buildings;}
+    public int getHuntersNumber() {return huntersNumber;}
+    public int getBuilderDiscount() {return builderDiscount;}
+    public int getGatherersDiscount() {return gatherersDiscount;}
+    public int getArtistsNumber() {return artistsNumber;}
+    public int getShamanStars() {return shamanStars;}
+    public int getInventorsNumberPerType() {return inventorsNumberPerType;}
 }
