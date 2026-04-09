6749c00ce9324fc00118c98f43f7ce97d216a79f
Mazzu <lorenzo.mazzucato@mail.polimi.it>
2026-04-09 11:15:11 +0200

    game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,33 +1,87 @@
-import enums.GamePhase;
-import users.Player;
-
 import java.util.ArrayList;
+import java.util.Collections;
+import users.Player;
+import game_board.OfferTrack;
+import enums.GamePhase;
+
+
 
 public class Game {
   
-  private ArrayList<Player> players;
-  private int numPlayers;
+  final private ArrayList<Player> players;
+  final private int numPlayers;
   private Player currentPlayer;
   private ArrayList<Player> turnOrder;
   private int era;
   private int currentRound;
   private GamePhase currentPhase;
+  private OfferTrack offertrack;
   
   /*private string game_id;
   public Game(string game_id){
     this.game_id=game_id;
   }*/
-  
-  private Game(){
+
+  public Game(ArrayList<Player> players) {
+      this.players = players;
+      this.numPlayers = players.size();
   }
+
+  public GamePhase getGamePhase(){return currentPhase}
+  public Player getCurrentPlayer(){return currentPlayer}
+  public int getNumPlayer(){return numPlayers}
+  private Player getNextPlayer(){
+      int i=0;
+      while(turnOrder.get(i)!=currentPlayer){
+          i++;
+      }
+      return turnOrder.get(i+1);
+  }
+  public ArrayList<Player> getTurnOrder(){return turnOrder}
+
+
+
   public void startGame(){
-    /*
-    aspetta client con gameid
-    fagli sciegliere colore
-    assegna ai giocatori turnOrder
-    inizializza offer tile in base a num giocatori/fanno loro
-        
-    */
+
+      turnOrder = new ArrayList<>(players);
+      Collections.shuffle(turnOrder);
+
+      currentPlayer=turnOrder.get(0);
+      currentRound=1;
+      era=1;
+
+      offertrack=new OfferTrack(numPlayers);
+
+      currentPhase=GamePhase.START_TURN;
   }
-  
+
+
+  public void finishGame(){
+      if(currentRound == 10){
+
+      }
+  }
+
+  public void setCurrentPlayer(){
+      currentPlayer=getNextPlayer();
+  }
+  public void updateCurrentPhase(){
+
+
+  }
+  public void checkEra(){
+
+  }
+
+  public void changeEra(){
+      era=era++;
+  }
+
+  public int getEra(){
+      return era;
+  }
+
+
+
+
 }
