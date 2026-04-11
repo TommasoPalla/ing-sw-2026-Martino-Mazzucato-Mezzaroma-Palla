8263e545e56fa884b6fd6d36359ccfb01244e1b9
PaoloMartino <paolo2.martino@mail.polimi.it>
2026-04-11 15:02:12 +0200

    initTribeDeck method added + allCards lists

--- src/main/java/cards_and_deck/Deck.java
+++ src/main/java/cards_and_deck/Deck.java
@@ -3,31 +3,66 @@ package cards_and_deck;
 import game.Game;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
     private ArrayDeque<Card> tribeDeck;
     private ArrayDeque<BuildingCard> buildingsDeck;
-    private int deckInitLength;
+    private int[] buildingsDeckLenght;  //Number of buildingCards for each era
+
+    // capire se metterli qua o in Game
+    private final List<CharacterCard> allCharacterCards;
+    private final List<EventCard> allEventCards;
+    private final List<BuildingCard> allBuildingCards;    //from JSON
 
     //constructor called by game.Game.startGame()
     public Deck(int numPlayers){
         switch (numPlayers){
-            case 2 -> this.deckInitLength = 70;//numero da definire
-            case 3 -> this.deckInitLength = 75;//num
-            case 4 -> this.deckInitLength = 80;//num
-            case 5 -> this.deckInitLength = 85;//num
+            case 2 -> this.buildingsDeckLenght = new int[]{1, 2, 3};
+            case 3 -> this.buildingsDeckLenght = new int[]{2, 2, 4};
+            case 4 -> this.buildingsDeckLenght = new int[]{2, 3, 4};
+            case 5 -> this.buildingsDeckLenght = new int[]{2, 3, 5};
         }
+        //da revisionare, scegliere se fare 3 file diversi per ogni tipo di carta
+        CardLoader loader = new CardLoader();
+        this.allCharacterCards = loader.loadCharacters("cards.json");    //card Loader
+        this.allEventCards = loader.loadEvents("cards.json");
+        this.allBuildingCards = loader.loadBuildings("cards.json");
         initTribeDeck(1);
+        initBuildingDeck(1);
     }
 
+    //getters
     public int getCardsNumber(){return tribeDeck.size();}
     public ArrayDeque<Card> getTribeDeck() {return tribeDeck;}
     public ArrayDeque<BuildingCard> getBuildingsDeck() {return buildingsDeck;}
 
     //method called by game.Game.changeEra(), creates a new deck for the current era
     public void initTribeDeck(int era){
+        List<Card> tempDeck = new ArrayList<>();
+        for (CharacterCard charCard: allCharacterCards){
+            if(charCard.getEra() == era){
+                tribeDeck.add(charCard);
+            }
+        }
+        for(EventCard evCard: allEventCards){
+            if(evCard.getEra() == era){
+                tribeDeck.add(evCard);
+            }
+        }
+        Collections.shuffle(tempDeck);
+        this.tribeDeck = new ArrayDeque<>(tempDeck);
     }
+
     public void initBuildingDeck(int era){
+        List<Card> tempDeck = new ArrayList<>();
+        for (CharacterCard charCard: allCharacterCards){
+            if(charCard.getEra() == era){
+                tribeDeck.add(charCard);
+            }
+        }
     }
 
     public Card drawCard(){
