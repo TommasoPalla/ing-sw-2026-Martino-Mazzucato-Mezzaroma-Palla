cac94d7545472040831a186ec97710782e4ef913
TommasoPalla <145765065+tommasopalla@users.noreply.github.com>
2026-04-21 13:45:17 +0200

    BuildingManagerTest started, some other changes
    Signed-off-by: TommasoPalla <145765065+TommasoPalla@users.noreply.github.com>

---
+++ src/test/java/it/polimi/ingsw/BuildingManagerTest.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw;
+
+import building_management.BuildingManager;
+import building_management.buildings.BonusPoints;
+import cards_and_deck.BuildingCard;
+import enums.Color;
+import enums.Effect;
+import enums.GamePhase;
+import enums.Parameters;
+import game.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import users.Player;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+public class BuildingManagerTest {
+    BuildingManager buildingManager = new BuildingManager();
+    EnumMap<Parameters, Integer> mapca =  new EnumMap<>(Parameters.class);
+    BuildingCard bonusPoints = new BonusPoints(2, "BP", 10, GamePhase.END_GAME,
+            Effect.BONUS_POINTS, "+25 points", 0);
+
+    @Test
+    public void buildingManagerTest() {
+        Player player1 =  new Player("giocatore1", Color.BLUE);
+        Player player2 =  new Player("giocatore2", Color.RED);
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        Game game = new Game(players);
+        player1.getTribe().modifyPrestigePoints(50);
+        player1.getTribe().addBuildingToTribe(bonusPoints);
+        assertEquals(player1.getTribe().getBuildings().getFirst(), bonusPoints);
+        assertEquals(bonusPoints.getOwner(), player1);
+        assertEquals(Game.getInstance().getBuildingManager().getBuildingsMap().get(bonusPoints.getActivatedAt()).get(player1).getFirst(), bonusPoints);
+        buildingManager.useBuilding(GamePhase.END_GAME, player1);
+        //assertEquals(player.getTribe().getPrestigePoints(), 75);
+    }
+}
