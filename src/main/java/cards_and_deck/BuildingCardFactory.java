9620795057e42851da0becc43aa1e0160662bb07
PaoloMartino <paolo2.martino@mail.polimi.it>
2026-04-21 15:47:38 +0200

    Buildings Factory's minor fix

--- src/main/java/cards_and_deck/BuildingCardFactory.java
+++ src/main/java/cards_and_deck/BuildingCardFactory.java
@@ -5,50 +5,65 @@ import enums.Effect;
 public class BuildingCardFactory {
     public static BuildingCard createBuilding(BuildingCardDTO dto) throws IllegalArgumentException{
         switch(dto.effect){
-            case Effect.ARTISTS_FOOD:
+            case Effect.ARTISTS_FOOD -> {
                 return new ArtistsFood(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.foodBonus);
-            case Effect.BONUS_POINTS:
+            }
+            case Effect.BONUS_POINTS -> {
                 return new BonusPoints(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestigeBonus);
-            case Effect.BONUS_STARS:
+            }
+            case Effect.BONUS_STARS -> {
                 return new BonusStars(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.starBonus);
-            case Effect.COMBO_FOOD:
+            }
+            case Effect.COMBO_FOOD -> {
                 return new ComboFood(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.foodBonus);
-            case Effect.COMBO_HUNTERS:
+            }
+            case Effect.COMBO_HUNTERS -> {
                 return new ComboHunters(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.foodBonus, dto.prestigeBonus);
-            case Effect.COMBO_POINTS:
+            }
+            case Effect.COMBO_POINTS -> {
                 return new ComboPoints(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.prestigeBonus);
-            case Effect.MULTIPLIER_RITUAL:
+            }
+            case Effect.MULTIPLIER_RITUAL -> {
                 return new MultiBonusRitual(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.multiplier);
-            case Effect.MULTIPLIER_BUILDER:
+            }
+            case Effect.MULTIPLIER_BUILDER -> {
                 return new MultiPointsBuilder(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.multiplier);
-            case Effect.DRAW_CARD:
+            }
+            case Effect.DRAW_CARD -> {
                 return new DrawAdditionalCard(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige);
-            case Effect.INVENTORS_FOOD:
+            }
+            case Effect.INVENTORS_FOOD -> {
                 return new InventorsFood(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.foodBonus);
-            case Effect.NO_MALUS_RITUAL:
+            }
+            case Effect.NO_MALUS_RITUAL -> {
                 return new NoMalusRitual(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige);
-            case Effect.POINTS_PER_ROLE:
+            }
+            case Effect.POINTS_PER_ROLE -> {
                 return new PointsPerRole(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.roleEffect, dto.prestigeBonus);
-            case Effect.SUSTENANCE_DISCOUNT:
+            }
+            case Effect.SUSTENANCE_DISCOUNT -> {
                 return new SustenanceDiscount(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.roleEffect, dto.foodDiscount);
-            case Effect.TOTEM_FOOD:
+            }
+            case Effect.TOTEM_FOOD -> {
                 return new TotemFood(dto.era, dto.cardID, dto.cost, dto.activatedAt,
                         dto.effect, dto.effectDescription, dto.prestige, dto.foodBonus);
-            default:
+            }
+            default -> {
                 throw new IllegalArgumentException("BuildingCard type does not exist\n");
+            }
         }
 
     }
