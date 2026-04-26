a65a98a8fc6e9da3180fffd83403e104f03ef1cf
Davide Mezzaroma <davidemezzaroma67@gmail.com>
2026-04-26 15:00:27 +0200

    Added runnable files (currently not working executing due to an error in CardLoader file). Added ServerConfigs, added explainations for ServerController

---
+++ src/main/java/it/polimi/ingsw/Apps/ClientApp.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.Apps;
+
+import it.polimi.ingsw.Networking.Configs.ServerConfigs;
+import it.polimi.ingsw.Networking.RMI.RMIServerAdapter;
+import it.polimi.ingsw.Networking.Shared.ServerConnection;
+import it.polimi.ingsw.Networking.Socket.SocketServerAdapter;
+
+import java.util.Scanner;
+
+/**
+ * This class must not have any parameters or methods. It is just an entry point
+ * It asks if you want to use RMI or TCP connection and creates the right adapter.
+ * It does the same thing for TUI or GUI
+ */
+public class ClientApp {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("choose connection protocol: Socket / RMI");
+        String protocol = scanner.nextLine();
+        ServerConnection connection;
+        //in futuro si puo' fare un do{}while che prende in input l'ip e la porta desiderata
+        //do{}while perche' si prova fintanto che sono ip e porta validi (=> helper class??)
+        switch (protocol.toUpperCase()){
+            case "SOCKET" -> connection = new SocketServerAdapter(ServerConfigs.DEFAULT_SOCKET_SERVER_IP_ADDR, ServerConfigs.DEFAULT_SOCKET_SERVER_PORT);
+            case "RMI" -> connection = new RMIServerAdapter(ServerConfigs.DEFAULT_RMI_SERVER_NAME, ServerConfigs.DEFAULT_RMI_SERVER_PORT);
+            default -> {
+                System.out.println("protocollo non valido");
+                return;
+            }
+        }
+        connection.connect();
+
+        //si fara' una cosa molto simile a quella sopra per istanziare TUI o GUI:
+        /*
+        System.out.println("choose visualization method:\n1 -> TUI\n2 -> GUI");
+        int view = scanner.nextInt();
+        scanner.nextLine();
+        View view;      //interfaccia implementata da GUI e TUI
+        switch(view){
+            ...
+        }
+        view.start();   //da qui si fa partire la GUI o la TUI e da li si prende l'input
+         */
+
+
+    }
+}
